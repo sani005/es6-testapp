e5852265a045d161480fca5baf199cee919665fa
Sani005 <anirai786@gmail.com>
2016-02-21 14:38:59 -0800

    First Commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+import Mortgage from './mortgage2';
+
+document.getElementById('calcBtn').addEventListener('click', () => {
+    let principal = document.getElementById("principal").value;
+    let years = document.getElementById("years").value;
+    let rate = document.getElementById("rate").value;
+    let mortgage = new Mortgage(principal, years, rate);
+
+    document.getElementById("monthlyPayment").innerHTML = mortgage.monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
+    mortgage.amortization.forEach(month => console.log(month));
+
+    let html = "";
+    mortgage.amortization.forEach((year, index) => html += `
+        <tr>
+          <td>${index + 1}</td>
+          <td class="currency">${Math.round(year.principalY)}</td>
+          <td class="stretch">
+            <div class="flex">
+                <div class="bar principal" style="flex:${year.principalY};-webkit-flex:${year.principalY}">
+                </div>
+                <div class="bar interest" style="flex:${year.interestY};-webkit-flex:${year.interestY}">
+                </div>
+            </div>
+          </td>
+          <td class="currency left">${Math.round(year.interestY)}</td>
+          <td class="currency">${Math.round(year.balance)}</td>
+        </tr>
+
+    `);
+    document.getElementById("amortization").innerHTML = html;
+
+
+
+
+});
